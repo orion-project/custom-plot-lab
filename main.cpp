0cf3d0e703c207d6d93db6326e698e30d8ab8a55
Nikolay Chunosov <N.Chunosov@yandex.ru>
2023-08-07 13:19:03 +0700

    add hidpi settings for sample project

--- main.cpp
+++ main.cpp
@@ -4,6 +4,10 @@
 
 int main(int argc, char *argv[])
 {
+#if (QT_VERSION < QT_VERSION_CHECK(6, 0, 0))
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling, true);
+    QCoreApplication::setAttribute(Qt::AA_UseHighDpiPixmaps, true);
+#endif
     QApplication a(argc, argv);
     a.setStyle("fusion");
     a.setOrganizationName("orion-project.org");
